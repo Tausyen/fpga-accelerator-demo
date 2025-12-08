726f6a7f5eb3c838aa5e1d3d8c35694c93159f09
its.tausyen <lzy0113tsy@outlook.com>
2025-12-08 14:34:43 +0800

    Update mac.v

--- project_A_mac/src/mac.v
+++ project_A_mac/src/mac.v
@@ -1,1 +1,13 @@
-
+// simple MAC (Multiply-Accumulate)
+module mac (
+  input clk,
+  input rst,
+  input signed [15:0] a,
+  input signed [15:0] b,
+  output reg signed [31:0] acc
+);
+  always @(posedge clk or posedge rst) begin
+    if (rst) acc <= 0;
+    else acc <= acc + a * b;
+  end
+endmodule
